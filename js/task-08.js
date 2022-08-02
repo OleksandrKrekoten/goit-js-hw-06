6fc5845bc1b93bcb40082ea1c12e783caf78daa7
Oleksandr <alexkrekoten11@gmail.com>
2022-08-02 15:31:54 +0300

    task-08/09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const  form = document.querySelector('.login-form')
+  
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+const {
+    elements: { email, password },
+} = event.currentTarget;
+
+if (email.value === "" || password.value === "") {
+ return alert("Будь ласка, заповніть усі поля");
+    }
+   
+
+  const result = { email: email.value, password: password.value };
+  console.log(result);
+  event.currentTarget.reset();
+}
+
+form.addEventListener("submit", onFormSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  changeColorBtn: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+  color: document.querySelector('.color')
+}
+refs.changeColorBtn.addEventListener('click', onChangeColor)
+
+function onChangeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor()
+  refs.color.textContent = getRandomHexColor()
+}
